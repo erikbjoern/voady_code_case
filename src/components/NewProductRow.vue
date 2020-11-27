06efdd8578f7240049898026eab88580040b6b3d
Erik Björn <erik.g.bjoern@gmail.com>
2020-11-27 16:42:57 +0100

    add comp. NewProductRow, merge Prod.TableHead into Prod.Inventory

---
+++ src/components/NewProductRow.vue
@@ -0,0 +1,88 @@
+<template>
+  <tr>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <input
+        class="text-sm font-medium text-gray-900 bg-transparent min-w-full"
+        v-model="name"
+      />
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <input
+        class="text-sm font-medium text-gray-900 bg-transparent min-w-full"
+        required
+        v-model="brand"
+      />
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap align-center">
+      <input
+        class="text-sm font-medium text-gray-900 bg-transparent numbers"
+        v-model="volume"
+        type="number"
+      />
+      <span class="text-gray-500">ml</span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap numbers-column">
+      <span class="text-gray-500">$</span>
+      <input
+        class="text-sm font-medium text-gray-900 bg-transparent numbers"
+        v-model="purchase_price"
+        type="number"
+      />
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap numbers-column">
+      <span class="text-gray-500">$</span>
+      <input
+        class="text-sm font-medium text-gray-900 bg-transparent numbers"
+        v-model="selling_price"
+        type="number"
+      />
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap numbers-column">
+      <input
+        class="text-sm font-medium text-gray-900 bg-transparent numbers"
+        v-model="balance"
+        type="number"
+      />
+      <span class="text-gray-500">st</span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <button class="shadow bg-yellow-400 px-4 py-2 text-white font-bold rounded">
+        Lägg till</button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "NewProductRow",
+  props: {
+    name: String,
+    brand: String,
+    volume: Number,
+    purchase_price: Number,
+    selling_price: Number,
+    balance: Number,
+  },
+};
+</script>
+
+<style scoped>
+input {
+  border-bottom: 1px solid lightgray;
+  width: 50px;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+
+.numbers-column {
+  text-align: right;
+}
+</style>
--- src/components/ProductInventory.vue
+++ src/components/ProductInventory.vue
@@ -17,12 +17,121 @@
     "
   >
     <template v-slot="{ result: { loading, error, data } }">
-      <product-table-head>
-        <div v-if="loading" class="apollo">Laddar . . .</div>
-        <div v-else-if="error" class="apollo">{{ error.message }}</div>
-        <product-table-body v-if="data.products" :products="data.products" />
-        <div v-else class="apollo">Inga produkter hittades</div>
-      </product-table-head>
+      <div class="flex flex-col" id="main-table-container">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div
+            class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
+          >
+            <div
+              class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+            >
+              <table
+                class="table-fixed min-w-full divide-y divide-gray-200 bg-gray-100"
+              >
+                <thead class="shadow-sm">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/3"
+                    >
+                      Namn
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/5"
+                    >
+                      Tillverkare
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+                    >
+                      Volym
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+                    >
+                      Inköpspris
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+                    >
+                      Försäljningspris
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+                    >
+                      Lagersaldo
+                    </th>
+                    <th scope="col" class="px-6 py-3 bg-gray-50">
+                      <svg
+                        v-if="!addProduct"
+                        class="edit shadow bg-yellow-400 rounded cursor-pointer p-1.5"
+                        @click="toggleDropDown()"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        viewBox="0 0 24 24"
+                        aria-haspopup="true"
+                        aria-expanded="true"
+                      >
+                        <path
+                          d="M18.31 2.828l2.862 2.862-15.032 15.032-3.583.722.723-3.585 15.03-15.031zm0-2.828l-16.872 16.872-1.438 7.128 7.127-1.437 16.873-16.874-5.69-5.689zm-.021 5.004l-12.728 12.728.707.708 12.728-12.728-.707-.708z"
+                        />
+                      </svg>
+                      <div
+                        v-show="dropDownOpen"
+                        class="origin-top-right absolute right-20 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="options-menu"
+                      >
+                        <div class="py-1">
+                          <a
+                            @click="toggleAddProduct()"
+                            href="#"
+                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                            role="menuitem"
+                            >Lägg till produkt</a
+                          >
+                        </div>
+                      </div>
+                      <button
+                        v-if="addProduct"
+                        class="shadow bg-gray-400 px-4 py-2 text-white font-bold rounded"
+                        @click="toggleAddProduct()"
+                      >
+                        Tillbaka
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody v-if="addProduct">
+                  <new-product-row />
+                </tbody>
+                <div v-else-if="loading" class="text-gray-500 p-4 border-none">
+                  Laddar . . .
+                </div>
+                <div v-else-if="error" class="text-gray-500 p-4 border-none">
+                  {{ error.message }}
+                </div>
+                <product-table-body
+                  v-else-if="data.products"
+                  :products="data.products"
+                  :dropDownOpen="dropDownOpen"
+                  :addProduct="addProduct"
+                />
+                <div v-else class="text-gray-500 p-4 border-none">
+                  Inga produkter hittades
+                </div>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </template>
   </apollo-query>
 </template>
@@ -30,10 +139,26 @@
 <script>
 import Vue from "vue";
 import ProductTableBody from "./ProductTableBody.vue";
-import ProductTableHead from "./ProductTableHead.vue";
+import NewProductRow from "./NewProductRow.vue";
+
 export default {
-  components: { ProductTableBody, ProductTableHead },
-  name: "ProductInventory"
+  components: { ProductTableBody, NewProductRow },
+  name: "ProductInventory",
+  data() {
+    return {
+      dropDownOpen: false,
+      addProduct: false,
+    };
+  },
+  methods: {
+    toggleDropDown: function() {
+      this.dropDownOpen = !this.dropDownOpen;
+    },
+    toggleAddProduct: function() {
+      this.addProduct = !this.addProduct;
+      this.dropDownOpen = false;
+    },
+  },
 };
 </script>
 
@@ -43,11 +168,8 @@ export default {
   margin: 5vw;
 }
 
-.apollo {
-  border-top: none;
-  color: #777;
-  padding: 10px;
-  width: inherit;
+.edit {
+  fill: white;
 }
 
 .numbers {
