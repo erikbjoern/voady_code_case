cb17cb827896d71315233b2afa7c44de5019c965
Erik Björn <erik.g.bjoern@gmail.com>
2020-11-29 00:27:18 +0100

    extract ProductTableHead, add authenticated as condition for rendering newProduct form

--- src/components/ProductInventory.vue
+++ src/components/ProductInventory.vue
@@ -28,98 +28,16 @@
               <table
                 class="table-fixed min-w-full divide-y divide-gray-200 bg-gray-100"
               >
-                <thead class="shadow-sm">
-                  <tr class="h-14">
-                    <th
-                      scope="col"
-                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/3"
-                    >
-                      Namn
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/5"
-                    >
-                      Artikelnummer
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/5"
-                    >
-                      Tillverkare
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
-                    >
-                      Volym
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
-                    >
-                      Inköpspris
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
-                    >
-                      Försäljningspris
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
-                    >
-                      Lagersaldo
-                    </th>
-                    <th scope="col" class="px-6 py-3 bg-gray-50">
-                      <svg
-                        v-if="authenticated && !addProduct"
-                        class="edit shadow bg-yellow-400 rounded cursor-pointer p-1.5"
-                        @click="toggleDropDown"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 24 24"
-                        aria-haspopup="true"
-                        aria-expanded="true"
-                      >
-                        <path
-                          d="M18.31 2.828l2.862 2.862-15.032 15.032-3.583.722.723-3.585 15.03-15.031zm0-2.828l-16.872 16.872-1.438 7.128 7.127-1.437 16.873-16.874-5.69-5.689zm-.021 5.004l-12.728 12.728.707.708 12.728-12.728-.707-.708z"
-                        />
-                      </svg>
-                      <div
-                        v-show="dropDownOpen"
-                        class="origin-top-right absolute right-20 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
-                        role="menu"
-                        aria-orientation="vertical"
-                        aria-labelledby="options-menu"
-                      >
-                        <div class="py-1">
-                          <a
-                            @click="toggleAddProduct"
-                            href="#"
-                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                            role="menuitem"
-                            >Lägg till produkt</a
-                          >
-                        </div>
-                      </div>
-                      <button
-                        v-if="addProduct"
-                        class="shadow bg-gray-400 px-4 py-2 text-white font-bold rounded"
-                        @click="toggleAddProduct"
-                      >
-                        Tillbaka
-                      </button>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody v-if="addProduct">
+                <product-table-head
+                  :authenticated="authenticated"
+                  :showNewProductForm="showNewProductForm"
+                  @toggleNewProductForm="showNewProductForm = !showNewProductForm"
+                />
+                <tbody v-if="showNewProductForm && authenticated">
                   <new-product-row :newProduct="newProduct" />
                 </tbody>
                 <form
-                  v-if="addProduct"
+                  v-if="showNewProductForm && authenticated"
                   id="addProduct"
                   @submit="submitNewProduct"
                 ></form>
@@ -132,8 +50,6 @@
                 <product-table-body
                   v-else-if="data && data.products !== {}"
                   :products="data.products"
-                  :dropDownOpen="dropDownOpen"
-                  :addProduct="addProduct"
                 />
                 <div v-else class="text-gray-500 p-4 border-none">
                   Inga produkter hittades
@@ -143,7 +59,7 @@
           </div>
         </div>
       </div>
-      <div v-if="error" class="ml-20 mt-2 text-red-900">
+      <div v-if="errorMessage" class="ml-20 mt-2 text-red-900">
         {{ graphqlError || "Något gick fel." }}
       </div>
     </template>
@@ -151,17 +67,18 @@
 </template>
 
 <script>
+import ProductTableHead from "./ProductTableHead.vue";
 import ProductTableBody from "./ProductTableBody.vue";
 import NewProductRow from "./NewProductRow.vue";
 import ADD_PRODUCT from "../queries/addProduct.gql";
 
 export default {
   name: "ProductInventory",
-  components: { ProductTableBody, NewProductRow },
+  components: { ProductTableBody, NewProductRow, ProductTableHead },
   data() {
     return {
-      dropDownOpen: false,
-      addProduct: false,
+      showDropdown: false,
+      showNewProductForm: false,
       newProduct: {
         name: "",
         id: "",
@@ -171,7 +88,7 @@ export default {
         selling_price: null,
         balance: null,
       },
-      error: null,
+      errorMessage: null,
     };
   },
   props: {
@@ -179,17 +96,10 @@ export default {
   },
   computed: {
     graphqlError: function() {
-      return this.error?.graphQLErrors[0]?.message || null
+      return this.errorMessage?.graphQLErrors[0]?.message || null;
     },
   },
   methods: {
-    toggleDropDown: function() {
-      this.dropDownOpen = !this.dropDownOpen;
-    },
-    toggleAddProduct: function() {
-      this.addProduct = !this.addProduct;
-      this.dropDownOpen = false;
-    },
     submitNewProduct: async function(event) {
       const e = event;
       event.preventDefault();
@@ -221,7 +131,8 @@ export default {
 
         e.target.reset();
       } catch (error) {
-        this.error = error;
+        this.errorMessage = error;
+        debugger
         console.log(error);
       }
     },
---
+++ src/components/ProductTableHead.vue
@@ -0,0 +1,113 @@
+<template>
+  <thead class="shadow-sm">
+    <tr class="h-14">
+      <th
+        scope="col"
+        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/3"
+      >
+        Namn
+      </th>
+      <th
+        scope="col"
+        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/5"
+      >
+        Artikelnummer
+      </th>
+      <th
+        scope="col"
+        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/5"
+      >
+        Tillverkare
+      </th>
+      <th
+        scope="col"
+        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+      >
+        Volym
+      </th>
+      <th
+        scope="col"
+        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+      >
+        Inköpspris
+      </th>
+      <th
+        scope="col"
+        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+      >
+        Försäljningspris
+      </th>
+      <th
+        scope="col"
+        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider numbers"
+      >
+        Lagersaldo
+      </th>
+      <th scope="col" class="px-6 py-3 bg-gray-50">
+        <svg
+          v-if="authenticated && !showNewProductForm"
+          class="edit shadow bg-yellow-400 rounded cursor-pointer p-1.5"
+          @click="toggleDropdown"
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          aria-haspopup="true"
+          aria-expanded="true"
+        >
+          <path
+            d="M18.31 2.828l2.862 2.862-15.032 15.032-3.583.722.723-3.585 15.03-15.031zm0-2.828l-16.872 16.872-1.438 7.128 7.127-1.437 16.873-16.874-5.69-5.689zm-.021 5.004l-12.728 12.728.707.708 12.728-12.728-.707-.708z"
+          />
+        </svg>
+        <div
+          v-show="showDropdown"
+          class="origin-top-right absolute right-20 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div class="py-1">
+            <a
+              @click="toggleNewProductForm"
+              href="#"
+              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+              >Lägg till produkt</a
+            >
+          </div>
+        </div>
+        <button
+          v-if="showNewProductForm && authenticated"
+          class="shadow bg-gray-400 px-4 py-2 text-white font-bold rounded"
+          @click="toggleNewProductForm"
+        >
+          Tillbaka
+        </button>
+      </th>
+    </tr>
+  </thead>
+</template>
+
+<script>
+export default {
+  name: "ProductTableHead",
+  data() {
+    return {
+      showDropdown: false,
+    }
+  },
+  props: {
+    authenticated: Boolean,
+    showNewProductForm: Boolean,
+  },
+  methods: {
+    toggleDropdown: function() {
+      this.showDropdown = !this.showDropdown;
+    },
+    toggleNewProductForm: function() {
+      this.showDropdown = false
+      this.$emit('toggleNewProductForm')
+    },
+  }
+}
+</script>
